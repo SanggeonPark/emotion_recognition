4491417bed1617612f791991b513a5245a90bd79
Sanggeon Park <gunnih@gmail.com>
2020-10-03 12:32:10 +0200

    added Constants, File Path and Label definition

---
+++ src/defines.py
@@ -0,0 +1,13 @@
+# Path
+xml_folder = "facial_emotion_data/train/annotations"
+train_image_folder = "facial_emotion_data/train/img"
+test_image_folder = "facial_emotion_data/test/img"
+
+# Constants
+input_shape_size = 28
+input_shape_2D = (input_shape_size, input_shape_size)
+input_shape_3D = (input_shape_size, input_shape_size, 1)
+
+#Labels
+
+emotion_labels = ['neutral', 'anger', 'surprise', 'smile', 'sad']
