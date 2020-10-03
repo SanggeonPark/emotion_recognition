fc8a7d12c8743b6b9ba612ef2765b3ac1ec02aaf
Sanggeon Park <gunnih@gmail.com>
2020-10-03 12:32:43 +0200

    added train data model

---
+++ src/models.py
@@ -0,0 +1,30 @@
+from switcher import l2i_switcher
+import declxml as xml
+
+# Dictionary Structure
+xml_preprocessor = xml.dictionary('annotation', [
+    xml.string('filename'),
+    xml.dictionary('size', [
+        xml.integer('width'),
+        xml.integer('height'),
+        xml.integer('depth')
+    ]),
+    xml.array(xml.dictionary('object', [
+        xml.string('name'),
+        xml.dictionary('bndbox', [
+            xml.integer('xmin'),
+            xml.integer('ymin'),
+            xml.integer('xmax'),
+            xml.integer('ymax')
+        ])
+    ]), alias='objects')
+])
+
+def dictionary_from_xml_file_url(xml_file_url):
+    return xml.parse_from_file(xml_preprocessor, xml_file_url)
+
+class MetaData:
+    def __init__(self, data, label):
+        self.data = data
+        switcher = l2i_switcher.get(label, lambda: "Invalid label")
+        self.label_number = switcher()
