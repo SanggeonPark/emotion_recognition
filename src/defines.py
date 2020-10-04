bad0c1324e1cb83455d7d128b7eae05f18409620
Sanggeon Park <gunnih@gmail.com>
2020-10-04 15:38:11 +0200

    added test out images.

--- src/defines.py
+++ src/defines.py
@@ -1,19 +1,24 @@
-# Path
+### Path
 xml_folder = 'facial_emotion_data/train/annotations'
 train_image_folder = 'facial_emotion_data/train/img'
 test_image_folder = 'facial_emotion_data/test/img'
 facial_emotion_recognition_model_path = 'facial_emotion_recognition.h5'
+output_folder = 'output/'
 
-#Emotion Labels
+### Emotion Labels
 emotion_labels = ['neutral', 'anger', 'surprise', 'smile', 'sad']
 
-# Constants
+### Constants
+
+# Shape
 input_shape_size = 48
 input_shape_2D = (input_shape_size, input_shape_size)
 input_shape_3D = (input_shape_size, input_shape_size, 1)
 
+# Classes
 num_classes = len(emotion_labels)
 
+# Filer and Pool
 num_filters = 8
 filter_size = 3
 pool_size = 2
--- src/test.py
+++ src/test.py
@@ -1,12 +1,29 @@
-from os import listdir
-from os.path import isfile, join
+from os import listdir, mkdir, remove
+from os.path import isfile, join, exists
 import numpy as np
 import cv2
 
 from models import getCNNModel
-from defines import emotion_labels, test_image_folder, input_shape_2D, facial_emotion_recognition_model_path
+from defines import emotion_labels, test_image_folder, output_folder, input_shape_2D, facial_emotion_recognition_model_path
 
-def test():
+def save_ouput(file_name, image, faces, labels):
+    output_image = image
+    color = (255, 0, 0)
+
+    start_points = []
+    for index in range(len(labels)):
+        (x,y,w,h) = faces[index]
+        start_point = (x, y)
+        start_points.append(start_point)
+        end_point = (x+w, y+h)
+        output_image = cv2.rectangle(output_image, start_point, end_point, color, 1)
+        output_image = cv2.putText(output_image,labels[index],start_point,cv2.FONT_HERSHEY_COMPLEX,1,color,2)
+
+    output_file_path = join(output_folder, file_name)
+    cv2.imwrite(output_file_path, output_image)
+    cv2.waitKey()
+
+def test(target_path):
     frontface_model_path = 'face_detection_data/haarcascade_frontalface_default.xml'
     profileface_model_path = 'face_detection_data/haarcascade_profileface.xml'
     frontface_cascade = cv2.CascadeClassifier(frontface_model_path)
@@ -15,15 +32,15 @@ def test():
     model = getCNNModel()
     model.load_weights(facial_emotion_recognition_model_path)
 
-    for f in listdir(test_image_folder): #load test image file names
+    for f in listdir(target_path): #load test image file names
         test_image_file_path = join(test_image_folder, f)
-        print(test_image_file_path)
-        gray_img = cv2.imread(test_image_file_path, cv2.IMREAD_GRAYSCALE)
-        faces = frontface_cascade.detectMultiScale(gray_img, 1.1, 4)
+        image = cv2.imread(test_image_file_path,cv2.IMREAD_UNCHANGED)
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = frontface_cascade.detectMultiScale(image, 1.1, 4)
         if (len(faces) == 0):
-            faces = profileface_cascade.detectMultiScale(gray_img, 1.1, 4)
+            faces = profileface_cascade.detectMultiScale(image, 1.1, 4)
         if (len(faces) == 0):
-            print('FAIL - No face found')
+            print('FAIL - No face found:', test_image_file_path)
             continue
 
         face_images = []
@@ -38,7 +55,18 @@ def test():
         predictions = model.predict(face_images)
         values = np.argmax(predictions, axis=1)
         labels = [emotion_labels[value] for value in values]
-        print('SUCCESS', labels)
+        print('SUCCESS', labels, test_image_file_path)
+
+        # draw rectangle on the face and print the emotion
+        save_ouput(f, image, faces, labels)
+
 if __name__ == "__main__":
-    # execute only if run as a script
-    test()
+
+    # clean up the output folder
+    if (exists(output_folder) == False):
+        mkdir(output_folder)
+    else:
+        for f in listdir(output_folder):
+            remove(join(output_folder, f))
+
+    test(test_image_folder)
